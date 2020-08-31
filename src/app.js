58a5c234edd1f19925b1a68041e3e7d165c1ed55
Umberto <umbertopellegrino96@gmail.com>
2020-08-31 16:44:08 +0200

    Nav bar e js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
+var $ = require( "jquery" );
+
+function openMenu() {
+  var menu = $('.fas.fa-bars.hamburger_dropdown_parent');
+
+  menu.click(function () {
+    if ($(".hamburger_dropdown").hasClass('open')) {
+      $(".hamburger_dropdown").removeClass('open');
+      $(".hamburger_dropdown").slideUp();
+    }else {
+      $(".hamburger_dropdown").slideDown();
+      $(".hamburger_dropdown").addClass('open');
+    }
+
+  });
+}
+
+
 function init() {
-  console.log('hello');
+  openMenu();
 }
 
 $(document).ready(init);
